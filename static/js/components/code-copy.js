49f9c9fa9ae1175584a1952c9c1670115be8229f
bryndin <1129396+bryndin@users.noreply.github.com>
2026-02-06 22:44:58 -0800

    Refactor code-copy, factor out CSS and JS

---
+++ static/js/components/code-copy.js
@@ -0,0 +1,39 @@
+const iconCopy =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/></svg>';
+const iconCheckmark =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/></svg>';
+
+document.querySelectorAll("pre > code").forEach((snippet) => {
+  // If line numbering is done via table, only use column with code
+  const tdCandidate = snippet.parentNode.parentNode;
+  if (tdCandidate.tagName === "TD" && tdCandidate.cellIndex !== 1) {
+    return;
+  }
+  const button = document.createElement("button");
+  button.classList.add("code-copy");
+  button.setAttribute("data-clipboard-snippet", "");
+  button.innerHTML = iconCopy;
+
+  const pre = snippet.parentNode;
+  pre.classList.add("parent-code-copy");
+  pre.insertBefore(button, snippet);
+});
+
+const clipboardSnippets = new ClipboardJS("[data-clipboard-snippet]", {
+  text: function (trigger) {
+    const code = trigger.nextElementSibling;
+    return code.textContent;
+  },
+});
+
+clipboardSnippets.on("success", function (e) {
+  const button = e.trigger;
+  button.innerHTML = iconCheckmark;
+  setTimeout(() => {
+    button.innerHTML = iconCopy;
+  }, 2000);
+});
+
+clipboardSnippets.on("error", function (e) {
+  console.error("Error copying to clipboard:", e);
+});
